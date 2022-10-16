eec0061d4ae8682fccfee7148adeb99eee4553ba
Felipe Coury <felipe.coury@gmail.com>
2022-10-15 22:52:37 -0300

    chore: don't use Path::new for basic tests

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,24 +1,24 @@
-use std::{fs, path::Path};
+use std::fs;
 
 use lol_inline_assets::inline;
 
 #[test]
 fn image() {
-    let inlined = inline(Path::new("tests/fixtures/image.html")).unwrap();
+    let inlined = inline("tests/fixtures/image.html").unwrap();
     let expected = fs::read_to_string("tests/fixtures/image.inlined.html").unwrap();
     assert_eq!(inlined, expected);
 }
 
 #[test]
 fn css() {
-    let inlined = inline(Path::new("tests/fixtures/css.html")).unwrap();
-    let expected = fs::read_to_string(Path::new("tests/fixtures/css.inlined.html")).unwrap();
+    let inlined = inline("tests/fixtures/css.html").unwrap();
+    let expected = fs::read_to_string("tests/fixtures/css.inlined.html").unwrap();
     assert_eq!(inlined, expected);
 }
 
 #[test]
 fn javascript() {
-    let inlined = inline(Path::new("tests/fixtures/javascript.html")).unwrap();
-    let expected = fs::read_to_string(Path::new("tests/fixtures/javascript.inlined.html")).unwrap();
+    let inlined = inline("tests/fixtures/javascript.html").unwrap();
+    let expected = fs::read_to_string("tests/fixtures/javascript.inlined.html").unwrap();
     assert_eq!(inlined, expected);
 }
